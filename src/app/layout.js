53a235bfcec22d1a88ea164e46c5818ceed5983b
josemmbcode <86318170+josemmbcode@users.noreply.github.com>
2023-02-14 13:45:25 -0600

    added dark mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,15 @@
 import Header from "@/components/Header";
+import Providers from "@/components/Providers";
 import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Header />
-        {children}
+        <Providers>
+          <Header />
+          {children}
+        </Providers>
       </body>
     </html>
   );
