4ad698d6940f80ca8098a7db494e8cefc7217337
josemmbcode <86318170+josemmbcode@users.noreply.github.com>
2023-02-20 12:32:33 -0600

    changed about page 2

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -4,12 +4,7 @@ export default function About() {
   return (
     <div className="max-w-6xl space-y-4 mx-auto px-4 ">
       <h1 className="text-2x; font-medium text-amber-600">About</h1>
-      <p>
-        As of March 2022, the database contained some 10.1 million titles,
-        including television episodes, and 11.5 million person records.
-        Additionally, the site had 83 million registered users. The site's
-        message boards were disabled in February 2017.
-      </p>
+      <p>The biggest movie databse.</p>
     </div>
   );
 }
