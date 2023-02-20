87c5097fdec114d23f3bdf4fc69bece789fe1d87
josemmbcode <86318170+josemmbcode@users.noreply.github.com>
2023-02-20 12:30:27 -0600

    CHANGED ABOUT PAGE

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -5,17 +5,6 @@ export default function About() {
     <div className="max-w-6xl space-y-4 mx-auto px-4 ">
       <h1 className="text-2x; font-medium text-amber-600">About</h1>
       <p>
-        IMDb (an abbreviation of Internet Movie Database) is an online database
-        of information related to films, television series, music videos,
-        podcasts, home videos, video games, and streaming content online –
-        including cast, production crew and personal biographies, plot
-        summaries, trivia, ratings, and fan and critical reviews. IMDb began as
-        a fan-operated movie database on the Usenet group "rec.arts.movies" in
-        1990, and moved to the Web in 1993. Since 1998, it is now owned and
-        operated by IMDb.com, Inc., a subsidiary of Amazon.{" "}
-      </p>
-
-      <p>
         As of March 2022, the database contained some 10.1 million titles,
         including television episodes, and 11.5 million person records.
         Additionally, the site had 83 million registered users. The site's
